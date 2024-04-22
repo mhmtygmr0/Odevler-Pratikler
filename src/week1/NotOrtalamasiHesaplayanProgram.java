1756d13ae45333f8597d62c354bd6fc4e65e1beb
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-22 17:24:14 +0300

    Hafta 1 eklendi

--- src/week1/NotOrtalamasiHesaplayanProgram.java
+++ src/week1/NotOrtalamasiHesaplayanProgram.java
@@ -2,6 +2,6 @@ package week1;
 
 public class NotOrtalamasiHesaplayanProgram {
     public static void main(String[] args) {
-        System.out.print("dwa");
+        System.out.print("dwaa");
     }
 }
