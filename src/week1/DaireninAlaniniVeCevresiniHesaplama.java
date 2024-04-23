5d9e7efc5e43a81f9fe551216a02e434505c17d1
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-23 21:31:15 +0300

    v1

--- src/week1/DaireninAlaniniVeCevresiniHesaplama.java
+++ src/week1/DaireninAlaniniVeCevresiniHesaplama.java
@@ -10,15 +10,22 @@ public class DaireninAlaniniVeCevresiniHesaplama {
         Scanner x = new Scanner(System.in);
 
         int yaricap;
-        double pi=3.14 , alan , cevre;
+        double pi=3.14 , alan , cevre , a ,dairedilimialan;
 
+        // Degiskenlere deger atayalim.
         System.out.print("Dairenin Yarı Çapını Giriniz : ");
         yaricap = x.nextInt();
 
+        System.out.print("Merkez açısının ölçüsünü giriniz: ");
+        a = x.nextDouble();
+
         alan = pi * yaricap * yaricap;
         cevre = 2 * pi * yaricap;
+        dairedilimialan = (pi * yaricap * yaricap * a) / 360;
 
+        // Sonucu ekrana yazdiralim.
         System.out.println("Dairenin Alanı = " + alan);
         System.out.println("Dairenin Çevresi = " + cevre + " cm");
+        System.out.println("Daire diliminin alanı: " + dairedilimialan);
     }
 }
