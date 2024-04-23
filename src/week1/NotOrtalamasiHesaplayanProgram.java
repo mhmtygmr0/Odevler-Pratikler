b3431ac1e62e007d96baa5ac5480578b4aeb81b4
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-23 12:59:18 +0300

    Not Ortalamsi Hesaplayan Program Oluşturuldu

--- src/week1/NotOrtalamasiHesaplayanProgram.java
+++ src/week1/NotOrtalamasiHesaplayanProgram.java
@@ -1,7 +1,45 @@
 package week1;
 
+// Kullanicidan giris almak icin Scanner sinifini projeye dahil etme.
+import java.util.Scanner;
+
 public class NotOrtalamasiHesaplayanProgram {
     public static void main(String[] args) {
-        System.out.print("dwaa");
+
+        //Scanner sinifimizi oluşturup ve x adında degişkenken tanimladim.
+        Scanner x = new Scanner(System.in);
+
+        // Değişkenleri oluştur.
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+        double ortalama;
+        String sonuc;
+
+        // Kullanicidan degerleri al.
+        System.out.print("Matematik Notunuz : ");
+        matematik = x.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = x.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya =  x.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce =  x.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih =  x.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik =  x.nextInt();
+
+        //ortlamayi hesapla ve yazdir.
+        ortalama = (matematik + fizik + kimya +turkce + tarih + muzik) / 6.0 ;
+        System.out.print( "Ortalamanız : " + ortalama);
+
+        //sonuc hesapla ve yazdir (gecti/kaldi).
+        sonuc = (ortalama >= 50.0) ? "Geçti" : "Kaldı" ;
+        System.out.print( " ===> " + sonuc);
+
     }
 }
