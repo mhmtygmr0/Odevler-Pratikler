b29bf5c9a2f8d7f0ab99cf405a14ad253aa803b8
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-23 17:58:26 +0300

    Taksimetre Programı

---
+++ src/week1/TaksimetreHesaplayanProgram.java
@@ -0,0 +1,23 @@
+package week1;
+
+// Kullanicidan giris almak icin Scanner sinifini projeye dahil etme.
+import java.util.Scanner;
+
+public class TaksimetreHesaplayanProgram {
+    public static void main(String[] args) {
+
+        //Scanner sinifimizi oluşturup ve x adında nesne tanimladim.
+        Scanner x = new Scanner(System.in);
+
+        //Degiskenlerimizi olusturalim.
+        double tutar,toplamtutar;
+        int mesafe , acilisUcreti = 10;
+
+        //Degiskenlere değer atayalim.
+        System.out.print("Gidilen Mesafeyi Giriniz (KM) : ");
+        mesafe = x.nextInt();
+        tutar = (mesafe * 2.20) + acilisUcreti;
+        toplamtutar = (tutar<20)? 20 : tutar;
+        System.out.print("Ödeyeceğiniz Toplam Tutar = " + toplamtutar + " TL");
+    }
+}
