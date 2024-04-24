cfa51eafee2d1be3f04eadc45195026d267fd2a1
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-24 11:31:23 +0300

    v2

---
+++ src/week1/VucutKitleIndeksiHesaplama.java
@@ -0,0 +1,10 @@
+package week1;
+
+import java.util.Scanner;
+
+public class VucutKitleIndeksiHesaplama {
+    public static void main(String[] args) {
+
+        Scanner x = new Scanner(System.in);
+    }
+}
