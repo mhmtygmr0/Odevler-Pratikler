9528e988c2ca8b42983b073e56abcd6af10851a2
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-04-24 11:42:10 +0300

    Vücut Kitle İndeksi Hesaplama

--- src/week1/VucutKitleIndeksiHesaplama.java
+++ src/week1/VucutKitleIndeksiHesaplama.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class VucutKitleIndeksiHesaplama {
     public static void main(String[] args) {
 
+        // Kullanicidan boy ve kilo bilgilerini alarak vucut kitle indeksini hesaplar.
+
         Scanner x = new Scanner(System.in);
+
+        // Degiskenlerimizi tanimliyoruz.
+        double m , kg , vki;
+
+        // Kullanicidan girdigimiz degerleri degiskenlerimize atiyoruz.
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        m = x.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kg = x.nextDouble();
+
+        // Vucut kitle indeksi hesaplayip ekrana yazdiriyoruz. Formül: kilo / (boy * boy)
+        vki = kg / (m * m);
+        System.out.print("Vücut Kitle İndeksiniz : " + vki);
     }
 }
