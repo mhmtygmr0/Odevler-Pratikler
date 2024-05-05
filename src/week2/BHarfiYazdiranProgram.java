e8851a812264c5d985168796f5ae40e32a015920
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-05-05 22:43:06 +0300

    "B" harfi yazan program

--- src/week2/BHarfiYazdiranProgram.java
+++ src/week2/BHarfiYazdiranProgram.java
@@ -1,24 +1,34 @@
 package week2;
 public class BHarfiYazdiranProgram {
     public static void main(String[] args) {
+
+        // B harfini oluşturmak için bir 2D dizi kullanılır.
         String[][] letter = new String[7][4];
 
+        // Döngülerle her bir hücreye "*" veya boşluk atanır, B harfine benzer bir şekil oluşturulur.
         for (int i = 0; i < letter.length; i++) {
 
             for (int j = 0; j < letter[i].length; j++) {
 
                 if (i == 0 || i == 3 || i == 6) {
+                    // Satırın üst, orta ve alt kısımlarında "*" bulunur.
                     letter[i][j] = " * ";
+
                 } else if (j == 0 || j == 3) {
+                    // Sol ve sağ köşelerde "*" bulunur.
                     letter[i][j] = " * ";
+
                 } else {
+                    // Diğer hücreler boşlukla doldurulur.
                     letter[i][j] = "   ";
+
                 }
 
             }
 
         }
 
+        // Oluşturulan B harfi ekrana basılır.
         for (String[] row : letter){
 
             for (String col : row){
@@ -29,20 +39,3 @@ public class BHarfiYazdiranProgram {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
