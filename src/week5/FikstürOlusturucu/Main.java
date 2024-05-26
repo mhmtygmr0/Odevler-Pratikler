d71ffe4aa4f295618b90cd07d0e3bdabd4bef53b
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-05-26 17:02:50 +0300

    v

--- src/week5/FikstürOlusturucu/Main.java
+++ src/week5/FikstürOlusturucu/Main.java
@@ -1,7 +1,18 @@
 package week5.FikstürOlusturucu;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> teams = new ArrayList<>();
+        teams.add("Galatasaray");
+        teams.add("Bursaspor");
+        teams.add("Fenerbahçe");
+        teams.add("Beşiktaş");
+        teams.add("Başakşehir");
+        teams.add("Trabzonspor");
 
+        Match scheduler = new Match(teams);
+        scheduler.generateFixtures();
     }
 }
